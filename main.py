b272100f7cb55203ffd4d3aa3d093e7c4fb8a590
thkim <thkim96@ssrinc.co.kr>
2024-11-18 19:41:51 +0900

    fastapi-ca 최초 커밋

---
+++ main.py
@@ -0,0 +1,15 @@
+# coding=utf-8
+import uvicorn
+
+from fastapi import FastAPI
+
+app = FastAPI()
+
+@app.get("/")
+def hello():
+    return {"Hello": "FastAPI"}
+
+
+
+if __name__ == '__main__':
+    uvicorn.run("main:app", host="127.0.0.1", reload=True)
