570b324d6c5bfcc350378879d2b323f4a3abab47
thkim <thkim96@ssrinc.co.kr>
2025-01-08 20:31:19 +0900

    250108-회원가입 인터페이스

--- main.py
+++ main.py
@@ -2,13 +2,23 @@
 import uvicorn
 
 from fastapi import FastAPI
+from user.interface.controllers.user_controller import router as user_routers
+from fastapi.exceptions import RequestValidationError
+from fastapi.requests import Request
+from fastapi.responses import JSONResponse
 
 app = FastAPI()
+app.include_router(user_routers)
 
-@app.get("/")
-def hello():
-    return {"Hello": "FastAPI"}
-
+@app.exception_handler(RequestValidationError)
+async def validation_exception_handler(
+        request: Request,
+        exc: RequestValidationError
+        ):
+    return JSONResponse(
+        status_code=400,
+        content=exc.errors(),
+        )
 
 
 if __name__ == '__main__':
