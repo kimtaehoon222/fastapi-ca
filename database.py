2f9e1cff758933950c36c0db2b6c1045824b8302
thkim <thkim96@ssrinc.co.kr>
2025-02-05 20:14:55 +0900

    SQLAlchemy ORM init

---
+++ database.py
@@ -0,0 +1,12 @@
+# coding=utf-8
+from sqlalchemy import create_engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+
+SQLALCHEMY_DATABASE_URL = "mysql+mysqldb://root:test@127.0.0.1/fastapi-ca"
+
+engine = create_engine(SQLALCHEMY_DATABASE_URL)
+
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bing=engine)
+
+Base = declarative_base()
